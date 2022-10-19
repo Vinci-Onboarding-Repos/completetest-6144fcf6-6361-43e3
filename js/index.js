2b6104b0886087b4ce879c4e3d49a38758ac2266
VINCIDAO <106922708+VINCIDAO@users.noreply.github.com>
2022-10-19 13:03:05 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -383,7 +383,7 @@ async function addxptopath(){
     const page = pathList[pathList.length - 1] == projectId ?
         "index" : pathList[pathList.length - 1];
 
-    const data = document.querySelector("#xp");
+    const data = document.querySelector("#complete");
     
     if (window.localStorage.getItem('user') === null) {
         var allIds = {};
